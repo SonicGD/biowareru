6380f32ff308a467782068834bc34ea81a11a169
SonicGD <sonicgd@gmail.com>
2014-07-07 14:25:28 +0600

    base structure

---
+++ common/config/main.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Георгий
+ * Date: 07.07.2014
+ * Time: 11:36
+ */
+
+return [];
---
+++ frontend/config/main.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Георгий
+ * Date: 07.07.2014
+ * Time: 11:37
+ */
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/params.php')
+);
+
+return [
+    'id'                  => 'app-frontend',
+    'basePath'            => dirname(__DIR__),
+    'bootstrap'           => ['log'],
+    'controllerNamespace' => 'bioengine\frontend\controllers',
+    'vendorPath'          => dirname(dirname(__DIR__)) . '/vendor',
+    'components'          => [
+        'user'         => [
+            'identityClass'   => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log'          => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets'    => [
+                [
+                    'class'  => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params'              => $params,
+];
