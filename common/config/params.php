da52be5f6e2844b5dbb7719bd3e82f42b4835d3a
SonicGD <sonicgd@gmail.com>
2014-12-10 14:46:30 +0500

    fixes

--- common/config/params.php
+++ common/config/params.php
@@ -1,10 +1,10 @@
 <?php
-
+defined('BW_ROOT') or define("BW_ROOT", realpath(__DIR__ . '/../../'));
 return [
     'db'                  => require_once('db.php'),
     'site_url'            => 'http://bw.localhost.ru',
-    'ipbwi_BOARD_PATH'    => '/var/www/biowareru/frontend/web/forum/',
-    'ipbwi_ROOT_PATH'     => '/var/www/biowareru/vendor/haslv/ipbwi/src/vendor/ipbwi/',
+    'ipbwi_BOARD_PATH'    => BW_ROOT . DIRECTORY_SEPARATOR . '/frontend/web/forum/',
+    'ipbwi_ROOT_PATH'     => BW_ROOT . DIRECTORY_SEPARATOR . '/vendor/haslv/ipbwi/src/vendor/ipbwi/',
     'ipbwi_WEB_URL'       => 'http://bw.localhost.ru',
     'ipbwi_COOKIE_DOMAIN' => '.bw.localhost.ru',
 ];
