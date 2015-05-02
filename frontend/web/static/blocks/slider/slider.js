24be47da00c70f93682c0e3e599cf6608773d64c
sladex <sladex@yandex-team.ru>
2015-05-02 16:34:23 +0500

    responsive slider without torchazhii konets

--- frontend/web/static/blocks/slider/slider.js
+++ frontend/web/static/blocks/slider/slider.js
@@ -1,13 +1,26 @@
 $(function () {
     var $carouselWrap = $('.slider'),
-        $carousel = $carouselWrap.find('.slider__list-items');
+        $carousel = $carouselWrap.find('.slider__list-items'),
+        $item = $carousel.find('.slider__list-item').eq(0),
+        carouselWrapPadding = $carouselWrap.outerWidth() - $carousel.innerWidth(),
+        itemWidth = $item.width(),
+        itemsMinNumber = 6,
+        itemsMaxNumber = 12,
+        responsive = { 0: { items: itemsMinNumber } },
+        breakpoint, i, steps;
+
+    for (i = 1, steps = itemsMaxNumber - itemsMinNumber; i < steps; i++) {
+        breakpoint = itemWidth * (itemsMinNumber + i) + carouselWrapPadding;
+        responsive[breakpoint] = { items: itemsMinNumber + i };
+    }
+
     $carousel.owlCarousel({
-        items: 12,
         loop: true,
         dots: false,
-        autoWidth: true
+        responsive: responsive
     });
-    $($carouselWrap).find('.slider__control_left').on('click', function() {
+
+    $carouselWrap.find('.slider__control_left').on('click', function() {
         $carousel.trigger('prev.owl.carousel');
     });
     $carouselWrap.find('.slider__control_right').on('click', function() {
