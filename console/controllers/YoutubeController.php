d0c632be597b6df11daacdd539d4625805a5ef81
SonicGD <sonicgd@gmail.com>
2015-06-14 16:04:09 +0500

    move echo

--- console/controllers/YoutubeController.php
+++ console/controllers/YoutubeController.php
@@ -38,11 +38,11 @@ class YoutubeController extends Controller
                     continue;
                 }
                 if ($file->size > 0 && $file->yt_id === '') {
-                    echo 'File ' . $file->title . ' starts' . PHP_EOL;
                     $info = pathinfo(parse_url($file->link, PHP_URL_PATH), PATHINFO_EXTENSION);
                     if (!in_array($info, $extensions, true)) {
                         continue;
                     }
+                    echo 'File ' . $file->title . ' starts' . PHP_EOL;
                     if (YoutubeHelper::upload($file)) {
                         echo 'File ' . $file->title . ' done' . PHP_EOL;
                     } else {
