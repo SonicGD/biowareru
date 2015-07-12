80c0a21f2529689d92b26560f07a8f848ce70523
SonicGD <sonicgd@gmail.com>
2015-07-12 15:58:56 +0500

    allow debug for localhost

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -87,7 +87,7 @@ $config = [
 if (YII_DEBUG) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+    $config['modules']['debug'] = ['class' => 'yii\debug\Module', 'allowedIPs' => ['127.0.0.1']];
 }
 
 return $config;
