74166d3529e0095a4b6053a5275e32e267126e1d
SonicGD <sonicgd@gmail.com>
2015-07-22 16:28:57 +0500

    fill meta tags for social networks

--- frontend/helpers/BioHtml.php
+++ frontend/helpers/BioHtml.php
@@ -3,12 +3,18 @@
 namespace biowareru\frontend\helpers;
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 class BioHtml extends Html
 {
     public static function entDecode($string)
     {
         return htmlspecialchars_decode($string);
+    }
+
+    public static function currentUrl()
+    {
+        return Url::current();
     }
 
 }
