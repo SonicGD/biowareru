4684fc0731ece2ceda569f81124da334a509de39
SonicGD <sonicgd@gmail.com>
2016-04-25 11:44:38 +0500

    ok, this way

--- frontend/static/blocks/sidebar/sidebar__counter.js
+++ frontend/static/blocks/sidebar/sidebar__counter.js
@@ -3,7 +3,7 @@ $(function () {
     var date = new Date(counter.text());
     counter.countdown(
         {until: date});
-    counter.show();
+    counter.css("display", "block");
 
     function serverTime() {
         var time = null;
