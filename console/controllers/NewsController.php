4913a69924afdc3b246a9a4f30f8271b592eac4b
SonicGD <sonicgd@gmail.com>
2016-07-03 00:15:29 +0500

    add yiisoft/httpclient and method to sync news with forum

--- console/controllers/NewsController.php
+++ console/controllers/NewsController.php
@@ -6,6 +6,8 @@ namespace biowareru\console\controllers;
 use bioengine\common\modules\ipb\models\IpbPost;
 use bioengine\common\modules\news\models\News;
 use yii\console\Controller;
+use yii\httpclient\Client;
+use yii\httpclient\Response;
 
 class NewsController extends Controller
 {
@@ -30,5 +32,118 @@ class NewsController extends Controller
                 var_dump($news->errors);
             }
         }
+    }
+
+    public function actionSyncForum($newsId)
+    {
+        /**
+         * @var News $news
+         */
+        $news = News::findOne($newsId);
+        if ($news) {
+
+            if (!$news->topic_id) {
+                //create new topic
+
+                $topic = [
+                    'forum'  => 4,
+                    'author' => $news->author_id,
+                    'title'  => $news->title,
+                    'post'   => $this->getPostContent($news),
+                    'hidden' => $news->pub ? 0 : 1
+                ];
+
+                $response = $this->doApiRequest("/forums/topics", $topic);
+                if ($response->isOk) {
+                    $news->topic_id = $response->data['id'];
+                    $news->pid = $response->data['firstPost']['id'];
+                }
+            } else {
+                //upd topic
+
+                $topic = [
+                    'title'  => $news->title,
+                    'hidden' => $news->pub ? 0 : 1
+                ];
+
+                $response = $this->doApiRequest("/forum/topics/" . $news->topic_id, $topic);
+                if ($response->isOk) {
+                    $post = [
+                        'post' => $this->getPostContent($news)
+                    ];
+                    $postResponse = $this->doApiRequest("/forum/posts/" . $news->pid, $post);
+                    if (!$postResponse->isOk) {
+                        //error
+                    }
+                } else {
+                    //error
+                }
+            }
+
+            if ($news->validate()) {
+                $news->save(false);
+            }
+        }
+    }
+
+    private $client;
+
+    private function getClient():Client
+    {
+        if (!$this->client) {
+            $this->client = new Client();
+        }
+        return $this->client;
+    }
+
+    private function doApiRequest($path, $data):Response
+    {
+        $client = $this->getClient();
+        $request = $client->createRequest()
+            ->setMethod('post')
+            ->setHeaders([
+                'Authorization' => 'Basic MTM2ODg3NjAxODU3ZTYyZTAzZGM4NDQ4NTg4MzdiNmE6'
+            ])
+            ->setUrl('http://ipb4.bioware.ru/api' . $path)
+            ->setData($data);
+        $response = $request->send();
+        return $response;
+    }
+
+
+    private function getPostContent(News $news):string
+    {
+        $postcontent = $news->short_text;
+
+        if (mb_strlen(trim($news->add_text)) > 0) {
+
+            $addText = <<<EOF
+                    <br />
+<div class="ipsSpoiler" data-ipsspoiler="">
+	<div class="ipsSpoiler_header">
+		<span>Скрытый текст</span>
+	</div>
+
+	<div class="ipsSpoiler_contents">
+		{$news->add_text}
+	</div>
+</div>
+EOF;
+
+
+            $postcontent .= $addText;
+        }
+
+
+        $postcontent = str_ireplace("<ul>", "<ul class=\"bbc\">", $postcontent);
+        $postcontent = str_ireplace("<ol>", "<ul class=\"bbcol decimal\">", $postcontent);
+        $postcontent = str_ireplace("</ol>", "</ul>", $postcontent);
+        $postcontent = preg_replace(
+            '#\[video id\=(([0-9]+)?) uri\=(.*?)\](.*?)\[/video\]#i',
+            "<div style=\"text-align:center\"><a href=\"" . $news->getPublicUrl() . "\">Посмотреть видео</a></div>",
+            $postcontent
+        );
+
+        return $postcontent;
     }
 }
